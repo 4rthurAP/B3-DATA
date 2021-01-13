3e5ff73d00da2f70a91bda343ac157ac19dbd4be
Arthur <68960780+4rthurAP@users.noreply.github.com>
2021-01-13 18:43:47 -0300

    create Temperatura-SP

---
+++ Temperatura-SP.py
@@ -0,0 +1,16 @@
+﻿# bibliotecas
+import bs4
+from bs4 import BeautifulSoup
+
+
+htmlText = """<div class="_flex _justify-center _align-center">
+<img width="60" height="60" src="/dist/images/v2/svg/2.svg">
+<span class="-bold -gray-dark-2 -font-55 _margin-l-20 _center">
+26º
+</span>
+</div>"""
+
+soup = BeautifulSoup(htmlText, features="lxml")
+spans = soup.findAll('span')
+for span in spans:
+    print(span.text)
